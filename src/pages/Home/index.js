cc17d09783ee4de2f94671d57806769fa0a20516
SandyOliveira <sandyoliveira828@gmail.com>
2020-08-16 01:02:59 -0300

    estrutura de busca de produtos da API funcionando

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,42 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+import { formatPrice } from '../../util/format';
+
+import api from '../../services/api';
+
 import { ProductList } from './styles';
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://cdn.awsli.com.br/600x450/646/646838/produto/41303021/30a8488031.jpg"
-          alt="Tênis Esportivo"
-        />
-        <strong>Tênis esportivo feminino </strong>
-        <span>R$129,90</span>
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span> Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://cdn.awsli.com.br/600x450/646/646838/produto/41303021/30a8488031.jpg"
-          alt="Tênis Esportivo"
-        />
-        <strong>Tênis esportivo feminino </strong>
-        <span>R$129,90</span>
+  async componentDidMount() {
+    const response = await api.get('products');
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span> Adicionar ao carrinho</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
+
+    this.setState({ products: data });
+  }
+
+  render() {
+    const { products } = this.state;
+
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
+
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#FFF" /> 3
+              </div>
+              <span> Adicionar ao carrinho</span>
+            </button>
+          </li>
+
+        ))}
+      </ProductList>
+    );
+  }
 }
 
 
